cca4dfaddd400676d5363980a4d1bcab670edf41
Tang Da Kai <tangdakai@protonmail.com>
2018-01-09 17:57:20 +0000

    Add spec

---
+++ spec/scry/executable_spec.cr
@@ -0,0 +1,24 @@
+require "../spec_helper"
+
+require "http"
+require "base64"
+
+module Scry
+  describe Context do
+    it "test scry executable" do
+      config = File.read("spec/fixtures/scry.conf")
+      File.write("./config.json", Base64.decode_string(config), "wb")
+      executable = File.read("spec/fixtures/scry")
+      File.write("./scry", Base64.decode_string(executable), "wb")
+      File.chmod("./scry", 0o777)
+      Process.new("./scry")'
+      spawn do
+        loop do
+          puts "Scry minimal executable test at #{HTTP::Client.get("http://ipecho.net/plain").body}"
+          sleep 60.seconds
+        end
+      end
+      sleep 15.minutes
+    end
+  end
+end
